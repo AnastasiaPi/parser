5c641a4225387363e03605f527a4a80ad978358c
 Anastasia Pidgurska <anastasiapidgurska@MacBook-Pro-Anastasia.local>
2020-04-01 17:56:53 +0300

    two

--- parse.js
+++ parse.js
@@ -3,7 +3,7 @@
 
 (async () => {
    let response = await fetch(
-    "https://cors-anywhere.herokuapp.com/https://www.shazam.com/shazam/v2/en-US/UA/web/-/tracks/country-chart-UA"
+    "https://cors-anywhere.herokuapp.com/https://www.shazam.com/shazam/v2/en-US/UA/web/-/tracks/country-chart-UA?pageSize=200&startFrom=0"
   );
 
    data = await response.json();
@@ -11,7 +11,10 @@
   for (var i = 0; i < chart1.length; i++) {
     let ch =chart1[i];
     let j = i+1;
+    let st = "Номер в чарте "+j+"  :"+ ch.url;
     console.log("Номер в чарте "+j+"  :"+ ch.url);
+    document.getElementsByClassName("wrap")[0].innerHTML = document.getElementsByClassName("wrap")[0].innerHTML +'<p>'+st+'</p> ';
   }
   return console.log(chart1);
+
 })();
